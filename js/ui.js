aa99a363352da8a261f54ea9b5745788268460a4
Ramiro Gómez <gomezramiro2002@gmail.com>
2021-04-22 21:54:39 -0300

    fix: card style

--- js/ui.js
+++ js/ui.js
@@ -8,16 +8,22 @@ export class UserInterface {
 		for (const card of cardsArray) {
 			const cardDiv = document.createElement('div');
 			cardDiv.classList.add('card');
+			// let cardSubName = `<p>${card.name}</p>`;
+			// if (card.name.length >= 11) {
+			// cardSubName = `<p class="f-size-large-text">${card.name}</p>`;
+			// }
 			cardDiv.innerHTML = `
 			<a>
-				<i class="fas fa-thumbtack"></i>
-			</a>
-			<div>
-				<h2>${card.id}</h2>
-				<p>${card.name}</p>
-			</div>
-			<p class="card-sub-text">1<span>${card.id}</span> = </p>
-			<h3>${card.value}<span>${base}</span></h3>
+                <i class="fas fa-thumbtack"></i>
+            </a>
+            <div class="card-name">
+                <h2>${card.id}</h2>
+                <p>${card.name}</p>
+            </div>
+            <div class="card-value">
+                <p>1<span>${card.id}</span> = </p>
+                <h3>${card.value}<span>${base}</span></h3>
+            </div>
 			`;
 			documentFragment.appendChild(cardDiv);
 		}
